1fae2f353801e63acc9c1cf6bcc647a061561650
cyri-sudo <116426089+cyri-sudo@users.noreply.github.com>
2022-10-22 08:35:36 +0100

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "HR6MQZ`+AH8fF_EGjDHy":
+            case "rockstar":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . f f f f f f f . . . . . 
+. . . f f f 5 5 5 5 f f . . . . 
+. . . f f 5 5 5 5 5 5 f . . . . 
+. . f f 5 5 5 5 5 5 5 f f . . . 
+. . f f f 2 2 2 2 2 f f f . . . 
+. . f 8 f b f 4 f b f 8 f . . . 
+. . f 8 4 1 f d f 1 4 8 f . . . 
+. . f 8 8 d d d d d 8 8 f . . . 
+. . f 8 8 8 4 4 4 8 8 8 f . . . 
+. . f 8 f 2 2 2 2 2 f 8 f . . . 
+. . f d f 2 2 2 2 2 f d f . . . 
+. . . 4 f 4 4 4 4 4 f 4 . . . . 
+. . . . . f f f f f . . . . . . 
+. . . . . f f . f f . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
